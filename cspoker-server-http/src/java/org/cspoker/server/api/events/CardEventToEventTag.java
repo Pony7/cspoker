e27a8889e2c40e8c8e02c6b30d4cc2a17c767e54
guyvdb <guyvdb@96f3ec31-5e34-0410-b54f-c54c24ea6f33>
2007-09-06 19:06:27 +0000

    moved http server to seperate project

---
+++ cspoker-server-http/src/java/org/cspoker/server/api/events/CardEventToEventTag.java
@@ -0,0 +1,70 @@
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+package org.cspoker.server.api.events;
+
+import java.util.List;
+
+import javax.xml.transform.sax.TransformerHandler;
+
+import org.cspoker.common.game.elements.cards.Card;
+import org.cspoker.server.game.events.Event;
+import org.cspoker.server.game.events.gameEvents.NewCommunityCardsEvent;
+import org.cspoker.server.game.events.gameEvents.privateEvents.NewPocketCardsEvent;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
+
+public class CardEventToEventTag extends EventToEventTag{
+
+    @Override
+    protected void addChildren(TransformerHandler response, Event event) throws SAXException{
+	super.addChildren(response, event);
+
+	List<Card> cards;
+	boolean privateCards;
+
+	if(event instanceof NewPocketCardsEvent){
+	    NewPocketCardsEvent cardEvent = (NewPocketCardsEvent)event;
+	    cards = cardEvent.getPocketCards();
+	    privateCards=true;
+	}else if(event instanceof NewCommunityCardsEvent){
+	    NewCommunityCardsEvent cardEvent = (NewCommunityCardsEvent)event;
+	    cards = cardEvent.getCommonCards();
+	    privateCards=false;
+	} else
+		throw new IllegalStateException("Event does not contain cards");
+	addCards(response, cards, privateCards);
+
+    }
+
+    private void addCards(TransformerHandler response, List<Card> cards, boolean privateCards) throws SAXException {
+	AttributesImpl attrs = new AttributesImpl();
+	if(privateCards){
+	    attrs.addAttribute("", "type", "type", "CDATA", "private");
+	}else{
+	    attrs.addAttribute("", "type", "type", "CDATA", "community");
+	}
+	response.startElement("", "cards", "cards", attrs);
+	for(Card card:cards){
+	    attrs = new AttributesImpl();
+	    attrs.addAttribute("", "suit", "suit", "CDATA", card.getSuit().toString());
+	    response.startElement("", "card", "card", attrs);
+	    String rank = card.getRank().toString();
+	    response.characters(rank.toCharArray(), 0, rank.length());
+	    response.endElement("", "card", "card");
+	}
+	response.endElement("", "cards", "cards");
+    }
+}
---
+++ cspoker-server-http/src/java/org/cspoker/server/api/httphandler/DealHandler.java
@@ -0,0 +1,59 @@
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+package org.cspoker.server.api.httphandler;
+
+
+import javax.xml.transform.sax.TransformerHandler;
+
+import org.cspoker.server.api.PlayerCommunicationFactory;
+import org.cspoker.server.api.httphandler.abstracts.HttpHandlerImpl;
+import org.cspoker.server.api.httphandler.abstracts.NoRequestStreamHandler;
+import org.cspoker.server.api.httphandler.exception.HttpSaxException;
+import org.cspoker.server.game.gameControl.IllegalActionException;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
+
+import com.sun.net.httpserver.HttpExchange;
+
+public class DealHandler extends NoRequestStreamHandler {
+
+    @Override
+    protected int getDefaultStatusCode() {
+	return 200;
+    }
+
+    @Override
+    protected void respond(TransformerHandler response, HttpExchange http) throws SAXException {
+	String username= HttpHandlerImpl.toPlayerName(http.getRequestHeaders());
+
+	try {
+	    PlayerCommunicationFactory.getRegisteredPlayerCommunication(username)
+	    .deal();
+	} catch (IllegalActionException e) {
+	    throw new HttpSaxException(e, 403);
+	}
+
+	try {
+	    response.startElement("", "ok", "ok", new AttributesImpl());
+	    response.endElement("", "ok", "ok");
+	} catch (SAXException e) {
+	    throw new HttpSaxException(e, 500);
+	}
+	
+    };
+
+
+}
