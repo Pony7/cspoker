383bc2c0ad4b30feb1731ac2e794c86a805a74c2
guyvdb <guyvdb@96f3ec31-5e34-0410-b54f-c54c24ea6f33>
2007-09-06 00:17:34 +0000

    Initial import.

---
+++ trunk/cspoker-server-sockets/src/java/org/cspoker/server/sockets/threading/LoggingThreadPool.java
@@ -0,0 +1,71 @@
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+package org.cspoker.server.sockets.threading;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.log4j.Logger;
+
+public class LoggingThreadPool extends ThreadPoolExecutor {
+
+    public LoggingThreadPool(int corePoolSize,
+	    int maximumPoolSize,
+	    long keepAliveTime,
+	    TimeUnit unit,
+	    BlockingQueue<Runnable> queue,
+	    String name) {
+	super(corePoolSize, maximumPoolSize, keepAliveTime, unit, queue);
+	setThreadFactory(new LoggingThreadFactory(name));
+    }
+
+    private final ThreadLocal<Long> startTime = new ThreadLocal<Long>();
+    private final AtomicLong numTasks = new AtomicLong();
+    private final AtomicLong totalTime = new AtomicLong();
+    private static Logger logger = Logger.getLogger(LoggingThreadPool.class);
+
+    protected void beforeExecute(Thread t, Runnable r) {
+	super.beforeExecute(t, r);
+	
+	logger.trace(String.format("Thread %s: start %s", t, r));
+	startTime.set(System.nanoTime());
+    }
+
+    protected void afterExecute(Runnable r, Throwable t) {
+	try {
+	    long endTime = System.nanoTime();
+	    long taskTime = endTime - startTime.get();
+	    numTasks.incrementAndGet();
+	    totalTime.addAndGet(taskTime);
+	    logger.trace(String.format("Thread %s: end %s, time=%dns",
+		    t, r, taskTime));
+	} finally {
+	    super.afterExecute(r, t);
+	}
+    }
+
+    protected void terminated() {
+	try {
+	    logger.trace(String.format("Terminated: avg time=%dns",
+		    totalTime.get() / numTasks.get()));
+	} finally {
+	    super.terminated();
+	}
+    }
+}
