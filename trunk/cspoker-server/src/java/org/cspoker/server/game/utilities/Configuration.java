054d28e2c8d47535eca1ca385ad95d66355571f2
guyvdb <guyvdb@96f3ec31-5e34-0410-b54f-c54c24ea6f33>
2007-09-29 12:11:12 +0000

    reverted to classpath-based configuration framework

--- trunk/cspoker-server/src/java/org/cspoker/server/game/utilities/Configuration.java
+++
@@ -1,7 +0,0 @@
-package org.cspoker.server.game.utilities;
-
-public class Configuration {
-
-    public static final String LOG4J_PROPERTIES = "cfg/org/cspoker/server/http/logging/log4j.properties";
-
-}
