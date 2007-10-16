4face9986a73cb6aff64124327887f1bcf0cec83
guyvdb <guyvdb@96f3ec31-5e34-0410-b54f-c54c24ea6f33>
2007-10-16 17:06:44 +0000

    it is now possible to instanciate multiple player factories

---
+++ cspoker-server/src/java/org/cspoker/server/common/threading/RequestExecutor.java
@@ -0,0 +1,37 @@
+package org.cspoker.server.common.threading;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class RequestExecutor implements Executor {
+
+    private LoggingThreadPool executor;
+
+    private RequestExecutor(){
+	executor = new LoggingThreadPool(
+		1,
+		Runtime.getRuntime().availableProcessors()+1, 
+		1, TimeUnit.SECONDS,
+		new PriorityBlockingQueue<Runnable>(1000, new SocketRunnableComparator()),
+		"TestServer"
+	);
+	executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+    }
+    
+    
+    public void execute(Runnable command) {
+	executor.execute(command);
+    }
+    
+    private static Executor instance=null;
+    
+    public synchronized static Executor getInstance(){
+	if(instance==null){
+	    instance = new RequestExecutor();
+	}
+	return instance;
+    }
+
+}
