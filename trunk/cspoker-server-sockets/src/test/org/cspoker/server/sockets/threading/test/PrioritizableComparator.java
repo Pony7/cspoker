2dcfd586b27fbbe73e4e96ec2d4c35c77b69db91
kenzotomita <kenzotomita@96f3ec31-5e34-0410-b54f-c54c24ea6f33>
2007-10-19 10:17:57 +0000

    Small changes for class name changes.

--- trunk/cspoker-server-sockets/src/test/org/cspoker/server/sockets/threading/test/PrioritizableComparator.java
+++ trunk/cspoker-server-sockets/src/test/org/cspoker/server/sockets/threading/test/PrioritizableComparator.java
@@ -3,12 +3,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
@@ -41,7 +41,7 @@ public class PrioritizableComparator extends TestCase {
 	queue.add(new TestPrioritizable(5, "runnable 11"));
 	queue.add(new TestPrioritizable(-2, "runnable 12"));
 	queue.add(new TestRunnable("runnable 13"));
-	
+
 	Runnable r; String names = "";
 	while((r = queue.poll()) !=null){
 	    names += r+", ";
@@ -61,17 +61,17 @@ public class PrioritizableComparator extends TestCase {
 			"runnable 12 with priority -2, "
 			, names);
     }
-    
+
     class TestPrioritizable implements Runnable, Prioritizable{
 
-	private int priority;
-	private String name;
+	private final int priority;
+	private final String name;
 
 	public TestPrioritizable(int priority, String name) {
 	    this.priority = priority;
 	    this.name = name;
 	}
-	
+
 	public void run() {
 	    // no op
 	}
@@ -79,23 +79,23 @@ public class PrioritizableComparator extends TestCase {
 	public int getPriority() {
 	    return priority;
 	}
-	
+
 	@Override
 	public String toString(){
 	    return name+" with priority "+priority;
 	}
-	
+
     }
-    
+
     class TestRunnable implements Runnable{
 
 	private int priority;
-	private String name;
+	private final String name;
 
 	public TestRunnable(String name) {
 	    this.name = name;
 	}
-	
+
 	public void run() {
 	    // no op
 	}
@@ -104,7 +104,7 @@ public class PrioritizableComparator extends TestCase {
 	public String toString(){
 	    return name;
 	}
-	
+
     }
 
 }
