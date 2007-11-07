5015fd037e6ac23e92c0486efacb20f608144a94
guyvdb <guyvdb@96f3ec31-5e34-0410-b54f-c54c24ea6f33>
2007-11-07 18:51:46 +0000

    Initial import.

---
+++ trunk/cspoker-server-rmi/src/java/org/cspoker/server/rmi/RemoteRMIServer.java
@@ -0,0 +1,12 @@
+package org.cspoker.server.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import org.cspoker.common.game.RemotePlayerCommunication;
+
+public interface RemoteRMIServer extends Remote{
+
+    public RemotePlayerCommunication login(String username, String password) throws RemoteException;
+    
+}
