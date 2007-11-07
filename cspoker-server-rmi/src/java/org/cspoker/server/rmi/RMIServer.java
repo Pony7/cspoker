2e15a8dd9a55fd2d282e68d30401a7fd3b4b53bb
guyvdb <guyvdb@96f3ec31-5e34-0410-b54f-c54c24ea6f33>
2007-11-07 18:51:46 +0000

    Initial import.

---
+++ cspoker-server-rmi/src/java/org/cspoker/server/rmi/RMIServer.java
@@ -0,0 +1,61 @@
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+package org.cspoker.server.rmi;
+
+import java.rmi.AccessException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+import org.cspoker.common.game.RemotePlayerCommunication;
+import org.cspoker.server.common.authentication.XmlFileAuthenticator;
+import org.cspoker.server.game.session.PlayerKilledExcepion;
+import org.cspoker.server.game.session.SessionManager;
+
+public class RMIServer implements RemoteRMIServer {
+
+    private final XmlFileAuthenticator authenticator;
+    private final int port;
+
+    public RMIServer(XmlFileAuthenticator authenticator, int port) {
+	this.authenticator = authenticator;
+	this.port = port;
+    }
+
+    public RemotePlayerCommunication login(String username, String password) throws RemoteException {
+	if(authenticator.hasPassword(username, password)){
+	    try {
+		RemotePlayerCommunication p = SessionManager.global_session_manager.getSession(username).getPlayerCommunication();
+		RemotePlayerCommunication stub=(RemotePlayerCommunication)UnicastRemoteObject.exportObject(p, 0);
+		return stub;
+	    } catch (PlayerKilledExcepion e) {
+		// bye bye bad client
+		return null;
+	    }
+	}else{
+	    throw new IllegalArgumentException("Login Failed");
+	}
+    }
+    
+    void start() throws AccessException, RemoteException {
+	System.setSecurityManager(null);
+	RemoteRMIServer stub=(RemoteRMIServer)UnicastRemoteObject.exportObject(this, 0);
+	Registry registry= LocateRegistry.getRegistry(port);
+	registry.rebind("CSPokerServer",stub);
+    }
+
+}
---
+++ cspoker-server-rmi/src/java/org/cspoker/server/rmi/RunRMIServer.java
@@ -0,0 +1,38 @@
+package org.cspoker.server.rmi;
+
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.cspoker.server.common.authentication.XmlFileAuthenticator;
+import org.cspoker.server.common.config.Log4JPropertiesLoader;
+
+public class RunRMIServer {
+
+    static{
+	Log4JPropertiesLoader.load("org/cspoker/server/rmi/logging/log4j.properties");
+    }
+    
+    private final static Logger logger = Logger.getLogger(RunRMIServer.class);
+        
+    public static void main(String[] args) throws NumberFormatException, IOException {
+	if (args.length < 1) {
+	    usage();
+	}
+
+	int port=0;
+	try {
+	    port=Integer.parseInt(args[0]);
+	} catch (NumberFormatException e) {
+	    usage();
+	}
+
+	RMIServer server = new RMIServer(new XmlFileAuthenticator(), port);
+	server.start();
+    }
+
+    private static void usage() {
+	logger.fatal("usage: java -jar cspoker-server-sockets.jar [portnumber]");
+	System.exit(0);
+    }
+    
+}
