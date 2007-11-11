384b3d3879b1e1b2a7b098bcca3c38ebcde1e3cd
kenzotomita <kenzotomita@96f3ec31-5e34-0410-b54f-c54c24ea6f33>
2007-11-11 21:06:26 +0000

    Initial commit.

---
+++ trunk/cspoker-common-rmi/src/java/org/cspoker/common/rmi/events/RemoteAllServerEventsListener.java
@@ -0,0 +1,36 @@
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+package org.cspoker.common.rmi.events;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import org.cspoker.common.game.events.serverEvents.PlayerJoinedEvent;
+import org.cspoker.common.game.events.serverEvents.PlayerLeftEvent;
+import org.cspoker.common.game.events.serverEvents.ServerMessageEvent;
+import org.cspoker.common.game.events.serverEvents.TableCreatedEvent;
+
+public interface RemoteAllServerEventsListener extends Remote{
+	
+	public void onPlayerJoinedEvent(PlayerJoinedEvent event) throws RemoteException;
+	
+	public void onPlayerLeftEvent(PlayerLeftEvent event) throws RemoteException;
+	
+    public void onTableCreatedEvent(TableCreatedEvent event) throws RemoteException;
+    
+    public void onServerMessageEvent(ServerMessageEvent event) throws RemoteException;
+
+}
