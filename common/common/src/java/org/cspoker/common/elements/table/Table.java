8a15e7bee13f6a2cf82d9799a2c5bc6f90aa46ac
kenzotomita <kenzotomita@96f3ec31-5e34-0410-b54f-c54c24ea6f33>
2008-02-29 23:49:36 +0000

    Immutable table object added.

---
+++ common/common/src/java/org/cspoker/common/elements/table/Table.java
@@ -0,0 +1,85 @@
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+package org.cspoker.common.elements.table;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+import org.cspoker.common.elements.GameProperty;
+import org.cspoker.common.player.Player;
+/**
+ * An immutable class to represent a snapshot of the state of a table.
+ * 
+ * @author kenzo
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Table {
+	
+	private TableId id;
+	
+	private String name;
+	
+	private List<Player> players;
+	
+	private boolean playing;
+	
+	private GameProperty property;
+	
+	public Table(TableId id, String name, Collection<Player> players, boolean playing, GameProperty property){
+		this.id = id;
+		this.name = name;
+		this.players = Collections.unmodifiableList(new ArrayList<Player>(players));
+		this.playing = playing;
+		this.property = property;
+	}
+	
+	protected Table(){
+		// no op
+	}
+	
+	public TableId getId(){
+		return id;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	
+	public List<Player> getPlayers(){
+		return players;
+	}
+	
+	public int getNbPlayers(){
+		return players.size();
+	}
+	
+	public boolean isPlaying(){
+		return playing;
+	}
+	
+	public GameProperty getGameProperty(){
+		return property;
+	}
+
+}
