83b77ebd82ce7c96cb12ff2b603bd020222410a8
cuypers.cedric <cuypers.cedric@96f3ec31-5e34-0410-b54f-c54c24ea6f33>
2008-03-01 20:29:48 +0000

    Fixed naming error and refactored inputs

--- trunk/server/xml/src/java/org/cspoker/server/xml/sockets/SocketServer.java
+++ trunk/server/xml/src/java/org/cspoker/server/xml/sockets/SocketServer.java
@@ -17,7 +17,6 @@
 package org.cspoker.server.xml.sockets;
 
 import java.io.IOException;
-import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
