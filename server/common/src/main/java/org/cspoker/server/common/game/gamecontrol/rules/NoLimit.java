998805710a6bc4a7537f6d0c22fedc3ca82d70c1
kenzotomita <kenzotomita@96f3ec31-5e34-0410-b54f-c54c24ea6f33>
2008-08-04 17:51:55 +0000

    betting rule test fixed

--- server/common/src/main/java/org/cspoker/server/common/game/gamecontrol/rules/NoLimit.java
+++ server/common/src/main/java/org/cspoker/server/common/game/gamecontrol/rules/NoLimit.java
@@ -1,6 +1,6 @@
 package org.cspoker.server.common.game.gamecontrol.rules;
 
-import org.cspoker.server.common.game.gamecontrol.rounds.Round;
+import org.cspoker.server.common.game.gamecontrol.rounds.BettingRound;
 
 /**
  * Class for the NoLimit betting game.rounds.rules used in Texas Hold'em.
@@ -19,7 +19,7 @@ public class NoLimit extends BettingRules {
 	 * Raise
 	 **************************************************************************/
 
-	public boolean isValidRaise(int amount, Round round) {
+	public boolean isValidRaise(int amount, BettingRound round) {
 		if (amount < getLastBetAmount()) {
 			setLastRaiseErrorMessage("ERROR : the bet amount must be at least the amount of the previous bet/raise"
 					+ " in the current round");
