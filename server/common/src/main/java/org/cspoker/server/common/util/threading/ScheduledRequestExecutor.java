833110a24502fbeb3840583a18801cb447e627e6
guyvdb <guyvdb@96f3ec31-5e34-0410-b54f-c54c24ea6f33>
2008-08-21 19:30:57 +0000

    Part of previous commit, forgot to remove bad methods

--- server/common/src/main/java/org/cspoker/server/common/util/threading/ScheduledRequestExecutor.java
+++ server/common/src/main/java/org/cspoker/server/common/util/threading/ScheduledRequestExecutor.java
@@ -16,17 +16,14 @@
 
 package org.cspoker.server.common.util.threading;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.AbstractExecutorService;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 public class ScheduledRequestExecutor extends AbstractExecutorService {
 
@@ -67,28 +64,6 @@ public class ScheduledRequestExecutor extends AbstractExecutorService {
 	public boolean awaitTermination(long timeout, TimeUnit unit)
 			throws InterruptedException {
 		return executor.awaitTermination(timeout, unit);
-	}
-
-	public <T> List<Future<T>> invokeAll(Collection<Callable<T>> tasks)
-			throws InterruptedException {
-		return executor.invokeAll(tasks);
-	}
-
-	public <T> List<Future<T>> invokeAll(
-			Collection<Callable<T>> tasks, long timeout, TimeUnit unit)
-			throws InterruptedException {
-		return executor.invokeAll(tasks, timeout, unit);
-	}
-
-	public <T> T invokeAny(Collection<Callable<T>> tasks)
-			throws InterruptedException, ExecutionException {
-		return executor.invokeAny(tasks);
-	}
-
-	public <T> T invokeAny(Collection<Callable<T>> tasks,
-			long timeout, TimeUnit unit) throws InterruptedException,
-			ExecutionException, TimeoutException {
-		return executor.invokeAny(tasks, timeout, unit);
 	}
 
 	public boolean isShutdown() {
