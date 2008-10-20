cf830ea09de041ac6ed1fced136a47f84cadc804
vomfeinsten@gmx.de <vomfeinsten@gmx.de@96f3ec31-5e34-0410-b54f-c54c24ea6f33>
2008-10-20 13:29:55 +0000

    Small fix, long instead of TableId was used for lookup. Getting contexts for Lobby, Table etc. from RemoteServerContext while providing the listeners on the client side does not seem to register the listeners yet, therefore we receive no events. Waiting for this to be fixed before continuing with testing.

--- server/common/src/main/java/org/cspoker/server/common/lobby/Lobby.java
+++ server/common/src/main/java/org/cspoker/server/common/lobby/Lobby.java
@@ -1,10 +1,6 @@
 package org.cspoker.server.common.lobby;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicLong;
@@ -23,7 +19,7 @@ import org.cspoker.server.common.account.ExtendedAccountContext;
 import org.cspoker.server.common.elements.id.TableId;
 import org.cspoker.server.common.gamecontrol.PokerTable;
 
-public class Lobby{
+public class Lobby {
 	
 	/**
 	 * Singleton lobby
@@ -36,79 +32,82 @@ public class Lobby{
 	private AtomicLong counter = new AtomicLong(0);
 	
 	private final List<LobbyListener> lobbyListeners = new CopyOnWriteArrayList<LobbyListener>();
-
+	
 	/**
 	 * The hash map containing all the tables of this lobby.
 	 */
 	private ConcurrentHashMap<TableId, PokerTable> tables = new ConcurrentHashMap<TableId, PokerTable>();
 	
-	protected Lobby(){
-		
-	}
+	protected Lobby() {
 
+	}
+	
 	public DetailedHoldemTable createTable(ExtendedAccountContext accountContext, String name,
 			TableConfiguration configuration) {
 		TableId tableId = new TableId(counter.getAndIncrement());
-		PokerTable table = new PokerTable(tableId, name, configuration, accountContext);		
+		PokerTable table = new PokerTable(tableId, name, configuration, accountContext);
 		tables.put(tableId, table);
 		
-		for(LobbyListener listener:lobbyListeners){
-			listener.onTableCreated(new TableCreatedEvent(accountContext.getPlayer().getMemento(), new Table(tableId.getId(), name)));
+		for (LobbyListener listener : lobbyListeners) {
+			listener.onTableCreated(new TableCreatedEvent(accountContext.getPlayer().getMemento(), new Table(tableId
+					.getId(), name)));
 		}
 		
 		return table.getTableInformation();
 	}
-
+	
 	public DetailedHoldemTable getTableInformation(long tableId) {
-		PokerTable table = tables.get(tableId);
-		return table!=null? table.getTableInformation(): null;
+		PokerTable table = tables.get(new TableId(tableId));
+		return table != null ? table.getTableInformation() : null;
 	}
-
+	
 	public TableList getTableList() {
 		Collection<PokerTable> currentTables = tables.values();
 		Set<Table> tableList = new TreeSet<Table>();
-		for(PokerTable table:currentTables){
+		for (PokerTable table : currentTables) {
 			tableList.add(table.getShortTableInformation());
 		}
 		return new TableList(new ArrayList<Table>(tableList));
 	}
-
-	public HoldemTableContext joinTable(TableId tableId, HoldemTableListener holdemTableListener, ExtendedAccountContext accountContext) throws IllegalActionException{
-		if(holdemTableListener==null)
+	
+	public HoldemTableContext joinTable(TableId tableId, HoldemTableListener holdemTableListener,
+			ExtendedAccountContext accountContext)
+			throws IllegalActionException {
+		if (holdemTableListener == null)
 			throw new IllegalArgumentException("The given holdem table listener is not effective.");
-		if(!tables.containsKey(tableId)){
-			throw new IllegalActionException("The provided table #"+tableId+" to join does not exist.");
+		if (!tables.containsKey(tableId)) {
+			throw new IllegalActionException("The provided table #" + tableId + " to join does not exist.");
 		}
 		PokerTable table = tables.get(tableId);
 		
 		return table.joinTable(accountContext.getPlayer(), holdemTableListener);
 	}
-
+	
 	public void removeTable(long tableId) {
 		PokerTable table = tables.get(tableId);
 		
-		if(table!=null && table.isEmpty()){
+		if (table != null && table.isEmpty()) {
 			tables.remove(tableId);
 		}
 		Table tableInfo = table.getShortTableInformation();
 		table.terminate();
-		for(LobbyListener listener:lobbyListeners){
+		for (LobbyListener listener : lobbyListeners) {
 			listener.onTableRemoved(new TableRemovedEvent(tableInfo));
 		}
 		
-		//TODO Concurrency issue: after removal joining table.
+		// TODO Concurrency issue: after removal joining table.
 	}
-
+	
 	public void subscribe(LobbyListener lobbyListener) {
 		lobbyListeners.add(lobbyListener);
 	}
-
+	
 	public void unSubscribe(LobbyListener lobbyListener) {
 		lobbyListeners.remove(lobbyListener);
 	}
 	
-	public static Lobby getInstance(){
+	public static Lobby getInstance() {
 		return lobby;
 	}
-
+	
 }
